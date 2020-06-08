b85792870d14252283f63bf574c8f1e5a96d9929
liqiwa <menghe163@gmail.com>
2020-06-08 16:07:10 +0800

    提交

--- bbs.py
+++ bbs.py
@@ -7,7 +7,7 @@ def home():
     return render_template('home.html')
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('register.html')
 @app.route('/login',methods=['POST'])
 def singin():
     #插入Mysql数据库中

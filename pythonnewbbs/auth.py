f50229901624641ca8b8b2617bcaaaca1e443dbe
liqiwa <menghe163@gmail.com>
2020-07-03 00:29:52 +0800

    update

--- pythonnewbbs/auth.py
+++ pythonnewbbs/auth.py
@@ -15,27 +15,31 @@ def register():
     # 插入Mysql数据库中
     print('注册页面识别成功')
     if request.method == 'POST':
-        #判断名称是否有重复，并在页面提示错误信息。
-
-
-        #判断 密码长度，邮箱格式是否正确，并在页面提示错误信息。
-
-
-        #无上述问题可以插入
         username = request.form['username']
         userpass = request.form['userpass']
         usermail = request.form['usermail']
-        userdate = datetime.datetime.now().replace(microsecond=0)
-        userlevel = '1'
-        new_users = Users(user_name=username,user_pass = generate_password_hash(userpass),user_email = usermail,
-                          user_date = userdate,user_level = userlevel)
+        #判断名称是否有重复，并在页面提示错误信息。
         dbsession = db.get_db()
-        dbsession.add(new_users)
-        dbsession.commit()
-        dbsession.close
-        print('555')
-        return redirect(url_for('auth.login'))
-        #增加 用户成功、失败、等新消息，
+        error = None
+        rows = dbsession.query(Users).filter(Users.user_name == username).first()
+        if not username:
+            error = '用户名必填'
+        elif not userpass:
+            error = '密码必填'
+        elif  rows is not None:
+            error = '用户名已经存在，请重新输入'
+        if error is None:
+            userdate = datetime.datetime.now().replace(microsecond=0)
+            userlevel = '1'
+            new_users = Users(user_name=username,user_pass = generate_password_hash(userpass),user_email = usermail,
+                              user_date = userdate,user_level = userlevel)
+            dbsession = db.get_db()
+            dbsession.add(new_users)
+            dbsession.commit()
+            dbsession.close
+            return redirect(url_for('auth.login'))
+        flash(error)
+        #增加 用户成功、失败、等页面提示新消息，
         #页面访问判断跳转，在这里。
     return render_template('auth/register.html')
 #用户登陆判断
@@ -45,17 +49,8 @@ def login():
     print("5555555")
     if request.method == 'POST':
         username = request.form['username']
-        print("6666666")
-
-
-        print("8")
-
         password = request.form['password']
-        print("9")
-
         dbsession = db.get_db()
-        print("777777")
-
         error = None
         rows = dbsession.query(Users).filter(Users.user_name == username).first()
         #print('',rows.user_name,rows.user_pass)
@@ -69,7 +64,7 @@ def login():
             return redirect(url_for('index'))
         flash(error)
     return render_template('auth/login.html')
-
+#需要重点了解
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
@@ -77,7 +72,7 @@ def load_logged_in_user():
         g.user = None
     else:
          g.user = db.get_db().query(Users).filter(Users.user_id == user_id).first()
-
+#需要重点了解
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
--- pythonnewbbs/blog.py
+++ pythonnewbbs/blog.py
@@ -1,23 +1,75 @@
 #完成主题数据的维护和更新
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,abort,request,redirect,url_for,flash
 from pythonnewbbs import db
+from pythonnewbbs.db import Categories,Topics,Posts
 
 #声明蓝图
-bp  =Blueprint("blog",__name__,url_prefix="/blog")
+bp  =Blueprint("blog",__name__)
 
-@bp.route('/topic',methods = ['GET'])
+@bp.route('/topic',methods = ['GET','POST'])
 def topic():
-    return render_template('blog/topic.html')
+    #查询数据库中所有的类别，在页面选项中展示出来
+        if request.method == 'GET':
+            dbsession = db.get_db()
+            posts = dbsession.query(Categories.cat_name).all()
+        return  render_template('blog/topic.html',posts = posts)
+        if request.method == 'POST':
+            # topicsubject = request.form['topic_subject']
+            # topiccat = request.form['topic_cat']
+            # topicdate =
+            # topby = request.form['']
+            # postcontent = request.form['post_content']
+            #
 
-@bp.route('/category',methods = ['GET'])
+          return render_template('blog/topic.html')
+
+def get_post(id, check_author=True):
+    """Get a post and its author by id.
+
+    Checks that the id exists and optionally that the current user is
+    the author.
+
+    :param id: id of post to get
+    :param check_author: require the current user to be the author
+    :return: the post with author information
+    :raise 404: if a post with the given id doesn't exist
+    :raise 403: if the current user isn't the author
+    """
+    dbsession = db.get_db()
+    post = (
+    dbsession.query(db.Topics).all()
+    )
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and post["author_id"] != g.user["id"]:
+        abort(403)
+
+    return post
+
+@bp.route('/category',methods = ['GET','POST'])
 def category():
+
+    if request.method == 'POST':
+        catname = request.form['catname']
+        catdescription = request.form['catdescription']
+        dbsession = db.get_db()
+        camg = dbsession.query(Categories).filter(Categories.cat_name == catname).first()
+        error = None
+        if camg is not None:
+            error='分类信息已经存在'
+        if error is None:
+            newCat = Categories(cat_name = catname,cat_description = catdescription)
+            dbsession.add(newCat)
+            dbsession.commit()
+            dbsession.close
+            return redirect(url_for('blog.category'))
+        flash(error)
     return render_template('blog/category.html')
+
 @bp.route('/')
 def index():
     dbsession = db.get_db()
-    posts = dbsession.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
+    posts = dbsession.query(db.Topics).all()
     return render_template('blog/index.html', posts=posts)

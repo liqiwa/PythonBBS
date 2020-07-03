d638af1d267ca86fea940a5e0129122a7c7e6de7
liqiwa <menghe163@gmail.com>
2020-07-04 00:45:50 +0800

    增加主题，同步发帖

--- pythonnewbbs/blog.py
+++ pythonnewbbs/blog.py
@@ -1,5 +1,6 @@
 #完成主题数据的维护和更新
-from flask import Blueprint, render_template,abort,request,redirect,url_for,flash
+import datetime
+from flask import Blueprint, render_template,abort,request,redirect,url_for,flash,session,g
 from pythonnewbbs import db
 from pythonnewbbs.db import Categories,Topics,Posts
 
@@ -10,18 +11,43 @@ bp  =Blueprint("blog",__name__)
 def topic():
     #查询数据库中所有的类别，在页面选项中展示出来
         if request.method == 'GET':
+            error = None
             dbsession = db.get_db()
-            posts = dbsession.query(Categories.cat_name).all()
-        return  render_template('blog/topic.html',posts = posts)
+            posts = dbsession.query(Categories).all()
+            #没有创建类别需提示，锁定提交按钮为灰色，或者弹出按钮后，回到首页。
+            if  len(posts) == 0:
+                error = '类别为空，联系管理员添加类别'
+            else:
+                return  render_template('blog/topic.html',posts = posts)
+            flash(error)
         if request.method == 'POST':
-            # topicsubject = request.form['topic_subject']
-            # topiccat = request.form['topic_cat']
-            # topicdate =
-            # topby = request.form['']
-            # postcontent = request.form['post_content']
-            #
-
-          return render_template('blog/topic.html')
+            error = None
+            topicsubject = request.form['topic_subject']
+            topiccat = request.form['topic_cat']
+            topicdate = datetime.datetime.now().replace(microsecond=0)
+            #获得用户信息
+            if g.user is None:
+                error ='请登陆后发布帖子'
+            else:
+                topicby = session['user_id']
+                postcontent = request.form['post_content']
+                postdate = datetime.datetime.now().replace(microsecond=0)
+                postby = topicby
+                if error is None:
+                    dbsession = db.get_db()
+                    newtopic = Topics(topic_subject = topicsubject,topic_date = topicdate,topic_cat = topiccat,
+                                      topic_by = topicby)
+                    dbsession.add(newtopic)
+                    #add 后可以直接查询ID 信息
+                    dbsession.flush()
+                    newpost = Posts(post_content = postcontent,post_date = postdate,post_topic = newtopic.topic_id,
+                                    post_by = postby)
+                    dbsession.add(newpost)
+                    dbsession.commit()
+                    dbsession.close
+                    return redirect(url_for('blog.topic'))
+            flash(error)
+        return render_template('blog/topic.html')
 
 def get_post(id, check_author=True):
     """Get a post and its author by id.
@@ -64,6 +90,7 @@ def category():
             dbsession.add(newCat)
             dbsession.commit()
             dbsession.close
+
             return redirect(url_for('blog.category'))
         flash(error)
     return render_template('blog/category.html')
